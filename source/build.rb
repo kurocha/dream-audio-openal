4653e6b7217775d0223077cd19d0431c8958a7a4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-09-09 22:16:56 +1200

    Migration of audio code into own library.

---
+++ source/build.rb
@@ -0,0 +1,12 @@
+
+compile_library 'DreamAudioOpenAL' do
+	def source_files(environment)
+		FileList[root, 'Dream/**/*.{cpp,m,mm}']
+	end
+end
+
+copy_headers do
+	def source_files(environment)
+		FileList[root, 'Dream/**/*.{h,hpp}']
+	end
+end
